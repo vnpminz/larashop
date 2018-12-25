882ce2ddb4ce267b790103dfdde9b8605cbe1c8f
vnpminz <tuanbui.n9@gmail.com>
2018-12-26 01:42:47 +0700

    ll

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'email' => 'abcd8@gmail.com',
+            'password'=> bcrypt('123456'),
+            'permission'=> 1
+        ];
+        DB::table('users')->insert($data);
+    }
+}
